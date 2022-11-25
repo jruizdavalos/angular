87de9c1033c0e57f03cda2047e975a5936634ff8
Juan Ruiz <jaruizdavalos@gmail.com>
2022-11-24 23:29:22 -0300

    Primer entregable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'class-01';
+  public title = 'class-01';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav/nav-bar/nav-bar.component';
+import { CarouselComponent } from './menu/carousel/carousel.component';
+import { CardComponent } from './card/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    CarouselComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule
